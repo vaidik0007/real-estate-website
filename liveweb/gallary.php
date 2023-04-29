2f423496ee2302fd637ebc0cc9521f35995c1047
vaidik0007 <132152742+vaidik0007@users.noreply.github.com>
2023-04-29 21:19:52 +0530

    Add files via upload

---
+++ liveweb/gallary.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+include 'menu.php'; ?>
+
+<section class="my-5">
+    <div class="py-5">
+        <h2 class="text-center">
+            PROJECT DONE 
+        </h2>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-12">
+                <img src="pexels-max-rahubovskiy-7031407.jpg" class="img-fluid pb-4">
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+                <img src="pexels-engin-akyurt-1463917.jpg" class="img-fluid pb-4">
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+                <img src="pexels-fuzail-ahmad-2261678.jpg" class="img-fluid pb-4">
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+                <img src="pexels-md-amir-umar-6161340.jpg" class="img-fluid pb-4">
+            </div>
+            <div class="col-lg-4 col-md-4 col-12 ">
+                <img src="pexels-bhupinder-singh-13870968.jpg" class="img-fluid pb-4">
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+                <img src="pexels-umar-andrabi-14926219.jpg" class="img-fluid pb-4">
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+                <img src="pexels-yogendra-singh-1542620.jpg" class="img-fluid pb-4">
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+                <img src="pexels-fuzail-ahmad-2261678.jpg" class="img-fluid pb-4">
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+                <img src="pexels-quang-nguyen-vinh-2138126.jpg" class="img-fluid pb-4">
+            </div>
+            <div class="text-center mx-auto">
+                <a href="index.php" class="btn btn-success">show less</a>
+                </div>
+
+        </div>
+    </div>    
+
+</section>
+</body>
+</html>
